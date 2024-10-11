a914e698410ca5bd9c67d50c2ee74978e657ed3d
Nicholas Davi <djnicolaz10@gmail.com>
2024-10-11 01:50:30 -0300

    Parei no exercicio 9

--- ConsoleApp4/Lista2.cs
+++ ConsoleApp4/Lista2.cs
@@ -78,27 +78,108 @@ namespace ConsoleApp4
 
         internal void Exercicio05()
         {
-            
+            string nome, sexo, estadoCivil, tempoCasada;
+
+            Console.WriteLine("Qual é seu nome: ");
+            nome = Console.ReadLine();
+            Console.WriteLine("Informe seu sexo (M)asculino ou (F)eminino: ");
+            sexo = Console.ReadLine();
+            Console.WriteLine("Insira seu estado civil (C)asado ou (S)oltero: ");
+            estadoCivil = Console.ReadLine();
+
+            if (sexo == "F" && estadoCivil == "CASADA")
+            {
+                Console.WriteLine("Há quantos você está casada? ");
+                tempoCasada = Console.ReadLine();
+                Console.WriteLine("Você está casada a " + tempoCasada + " anos");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Seus dados estão devidamente guardados");
+                Console.ReadLine();
+            }
         }
 
         internal void Exercicio06()
         {
-            throw new NotImplementedException();
+            int num1 = 0;
+
+            Console.WriteLine("Informe um número: ");
+            num1 = int.Parse(Console.ReadLine());
+
+            if (num1 % 2 == 0)
+            {
+                Console.WriteLine("Este número " + num1 + " é par");
+                Console.ReadLine();
+            }
+            else 
+            {
+                Console.WriteLine("Este número " + num1 + " é impar");
+                Console.ReadLine();
+            }
         }
 
         internal void Exercicio07()
         {
-            throw new NotImplementedException();
+            int a, b, c;
+
+            Console.WriteLine("Insira um valor para a:");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Insira um valor para b:");
+            b = int.Parse(Console.ReadLine());
+
+            if (a == b)
+            {
+                c = a + b;
+                Console.WriteLine("A soma dos dois números é: "+c);
+                Console.ReadLine();
+            }
+            else 
+            {
+                c = a * b;
+                Console.WriteLine("A multiplicação dos dois números é: " + c);
+                Console.ReadLine();
+            }
+            Console.WriteLine("O resultado é: " + c);
+            Console.ReadLine();
         }
 
         internal void Exercicio08()
         {
-            throw new NotImplementedException();
+            int num1, num2;
+
+            Console.WriteLine("Insira o número desejado: ");
+            num1 = int.Parse(Console.ReadLine());
+
+            if (num1 >= 1)
+            {
+                num2 = num1 * 2;
+                Console.WriteLine("Este número " + num2 + " é positivo");
+                Console.ReadLine();
+            }
+            else 
+            {
+                num2 = num1 * 3;
+                Console.WriteLine("Este número " + num2 + " é negativo");
+                Console.ReadLine();
+            }
+            Console.WriteLine("O Resultado é " + num2);
+            Console.ReadLine();
         }
 
         internal void Exercicio09()
         {
-            throw new NotImplementedException();
+            int valor1, valor2, valor3;
+
+            Console.WriteLine("Informe o primeiro valor: ");
+            valor1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Informe o segundo valor: ");
+            valor2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Informe o terceiro valor: ");
+            valor3 = int.Parse(Console.ReadLine());
+
+
         }
 
         internal void Exercicio10()
